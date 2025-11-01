1184f77ac5ab03eb193e30d72c15c4a6f8c82feb
OmarKh22 <ok597451@gmail.com>
2025-11-02 01:01:44 +0200

    starting routing & navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-5xl font-bold text-accent">Omar!</Text>
+      <Link href="/onboarding" className="mt-5 px-4 py-2 bg-primary rounded-md">
+        <Text className="text-white text-lg">Get Started</Text>
+      </Link>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,30 @@ import { Stack } from "expo-router";
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen
+      name="(tabs)"
+      options={{
+        headerShown: false
+      }}
+    />
+    <Stack.Screen
+      name="movie/[id]"
+      options={{
+        headerShown: false
+      }}
+    />
+    <Stack.Screen
+      name="(tabs)/profile"
+      options={{
+        headerShown: false
+      }}
+    />
+    <Stack.Screen
+      name="(tabs)/search"
+      options={{
+        headerShown: false
+      }}
+    />
+  </Stack>;
 }
